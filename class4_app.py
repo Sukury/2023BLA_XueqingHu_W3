318ae950a89769ffcbf65448985eaefdb3553e26
Sukury <xh2522@nyu.edu>
2023-09-27 18:06:21 -0400

    msg

--- class4_app.py
+++ class4_app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 
 st.title("Slider Test")
-st.text("use the slider tool in the streamlit library to create a slider tool for calculate the square of the input value")
+st.text("""
+use the slider tool in the streamlit library to create a slider tool for calculate the square of the input value
+""")
 
 x = st.slider("select a value")
 
